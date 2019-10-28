53b12008edcc7c5d5603c6e116c564c0b268db35
chris13012 <christophecaillet@icloud.com>
2019-10-28 10:47:04 +0100

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,86 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>ENGAGEMENT DE CONFIDENTIALITÉ GOALALA</p>
       </header>
+      <section>
+        La protection de votre vie privée est importante pour nous. C’est la
+        raison pour laquelle nous avons élaboré un Engagement de confidentialité
+        qui régit la manière dont nous recueillons, utilisons, divulguons,
+        transférons et stockons vos données personnelles. En plus de cet
+        Engagement de confidentialité, nous fournissons des informations sur les
+        données et la confidentialité, intégrées dans nos produits, pour
+        certaines fonctionnalités qui demandent à utiliser vos données
+        personnelles.Vous pouvez lire ces informations avant d’activer ces
+        fonctionnalités, dans les réglages associés ou en ligne. Veuillez
+        prendre le temps de vous familiariser avec nos pratiques en matière de
+        confidentialité et n’hésitez pas à nous contacter si vous avez des
+        questions.
+        <h3>Collecte et utilisation des données personnelles</h3>
+        Les données personnelles sont des informations pouvant être utilisées
+        pour identifier ou contacter une personne spécifique. Il peut vous être
+        demandé de communiquer vos données personnelles à tout moment lorsque
+        vous êtes en contact avec nous. Vous n’êtes pas obligé de nous
+        communiquer les données personnelles que nous vous demandons. Cependant,
+        si vous choisissez de ne pas le faire, nous ne pourrons pas toujours
+        vous fournir nos produits et services, ni même répondre à vos questions.
+        À des fins de recherche et développement, nous pouvons utiliser des
+        ensembles de données tels que ceux contenant des images, voix ou autres
+        données pouvant être associées à une personne identifiable. Lorsque nous
+        acquérons ce type d’ensemble de données, nous le faisons conformément à
+        la loi applicable dans la juridiction dans laquelle se trouve l’ensemble
+        de données. Quand nous utilisons ce type d’ensemble de données pour la
+        recherche et le développement, nous n’essayons pas d’identifier à
+        nouveau les personnes susceptibles d’y figurer.
+        <h3>Collecte et utilisation des données non personnelles</h3>
+        Nous collectons également des données dont la forme ne nous permet pas
+        de faire un rapprochement direct avec une personne en particulier. Nous
+        pouvons recueillir, utiliser, transférer et divulguer des données non
+        personnelles à quelque fin que ce soit. Ces informations peuvent être
+        utilisées pour améliorer la pertinence des résultats fournis par nos
+        services. Elles ne sont pas associées à votre adresse IP, excepté dans
+        de rares cas afin d’assurer la qualité de nos services sur Internet. -
+        Avec votre consentement explicite, nous pouvons collecter des données
+        sur la façon dont vous utilisez votre appareil et vos applications afin
+        d’aider les développeurs à améliorer leurs apps. Si nous associons des
+        données non personnelles à des données personnelles, les données ainsi
+        combinées sont traitées comme des données à caractère personnel tant
+        qu’elles restent associées.
+        <h3>Cookies et autres technologies</h3>
+        Les sites web, les services en ligne, les applications interactives, les
+        e-mails et les publicités peuvent utiliser des « cookies » et d’autres
+        technologies, telles que des « pixel tags » et des balises web. Ces
+        technologies nous permettent de mieux comprendre le comportement des
+        utilisateurs, nous indiquent quelles parties de nos sites web sont les
+        plus visitées, et facilitent et mesurent l’efficacité des publicités et
+        recherches Internet. Nous traitons les données collectées par les
+        cookies et autres technologies comme des données non personnelles.
+        Toutefois, si les adresses IP (Internet Protocol) ou des identifiants
+        similaires sont considérés comme des données personnelles par la loi
+        locale, nous traitons également ces identifiants comme des données
+        personnelles. De la même manière, si des données non personnelles sont
+        associées à des données personnelles, nous traitons les informations
+        ainsi associées comme des données personnelles aux fins du présent
+        Engagement de confidentialité.
+        <h3>Divulgation à des tiers</h3> Goalala peut parfois mettre certaines
+        données personnelles à la disposition de partenaires stratégiques
+        travaillant avec nous pour la fourniture de produits et services, ou
+        aidant Goalala à commercialiser ses produits auprès des clients. Les
+        données personnelles sont uniquement partagées par Goalala pour fournir
+        ou améliorer des produits, services et publicités ; elles ne sont pas
+        partagées avec des tiers à des fins marketing.{" "}
+        <h3>Protection des données personnelles</h3> Goalala prend la sécurité
+        de vos données personnelles très au sérieux. Les services en ligne
+        protègent vos données personnelles pendant le transit grâce au cryptage
+        TLS (Transport Layer Security). Lorsque vos données personnelles sont
+        stockées, nous utilisons des systèmes informatiques à accès limité
+        installés dans des locaux utilisant des mesures de sécurité physiques.
+      </section>
     </div>
   );
 }
