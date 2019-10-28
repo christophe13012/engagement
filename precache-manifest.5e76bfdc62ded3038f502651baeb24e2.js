bcc8e02a0961a71d3930365d1e3c437d3f846b00
chris13012 <christophecaillet@icloud.com>
2019-10-28 10:59:47 +0100

    Updates

---
+++ precache-manifest.5e76bfdc62ded3038f502651baeb24e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276af2de8e417ad0618b0563f644334f",
+    "url": "/engagement/index.html"
+  },
+  {
+    "revision": "986969440b36d5563329",
+    "url": "/engagement/static/css/main.f4d34bdc.chunk.css"
+  },
+  {
+    "revision": "ad3c08f8942902a201c3",
+    "url": "/engagement/static/js/2.1cf7dc77.chunk.js"
+  },
+  {
+    "revision": "986969440b36d5563329",
+    "url": "/engagement/static/js/main.ae34d1b9.chunk.js"
+  },
+  {
+    "revision": "4043b4a198ff77e28e9b",
+    "url": "/engagement/static/js/runtime-main.e3df26c6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/engagement/static/media/logo.25bf045c.svg"
+  }
+]);
